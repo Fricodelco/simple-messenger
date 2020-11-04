70f0b217767ac3df536e4429f809c73b2bf7ac73
Your Name <rodion_anisimov@mail.ru>
2020-11-04 09:18:25 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,67 @@
+#/home/rodion/python_virtual/env/bin/python3
+from flask import Flask, request, abort
+from datetime import datetime
+import time 
+app = Flask(__name__)
+
+db = [
+    {
+        'text': 'Привет',
+        'name': 'Nick',
+        'time': time.time()
+    }, {
+        'text': 'Привет, Nick',
+        'name': 'Jane',
+        'time': time.time()
+    }, {
+        'text': 'Как дела?',
+        'name': 'Nick',
+        'time': time.time()
+    }
+]
+
+@app.route("/")
+
+def hello():
+    return("hi <a href='/status'>Статистика</a>")
+
+@app.route("/status")
+def status():
+    return {
+        'name': 'post_msg',
+        'messages_count': 500,
+        'server_time': time.time(),
+        'status': True,
+        'time': datetime.now().isoformat()
+    }
+
+@app.route("/send", methods=['POST'])
+def send_message():
+    text = ''
+    name = ''
+    db.append({
+        'text': text,
+        'name': name,
+        'time': time.time()
+    })
+    return {'ok':True}
+
+@app.route("/messages")
+def get_messages():
+    if 'after' in request.args:
+        try:
+            after = float(request.args['after'])
+        except:
+            print('error')
+            return abort(400)
+    else:
+        after = 0 
+    filtered_db = []
+    for message in db:
+        if message['time'] > after:
+            filtered_db.append(message)
+            if len(filtered_db >= 100):
+                break
+    return {'messages': filtered_db}
+
+app.run()
